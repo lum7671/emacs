69649dd7c1509841b343dc7f97ec069c41ec4e5e
Nate Doohyun Jang <lum7671@me.com>
2015-11-28 01:39:04 +0900

    업데이트

--- init.el
+++ init.el
@@ -2,29 +2,38 @@
 ;; My own setting
 ;;
 
+
+;(setq default-directory "~/.emacs.d")
+;(setq user-emacs-directory "~/.emacs.d/lisp")
+;(setq diary-file "~/.emacs.d/diary")
+
+
+
+(require 'cask "~/.emacs.d/.cask/24.5.1/elpa/cask-20151123.528/cask.el")
+(cask-initialize)
+
+(let ((default-directory "/usr/local/share/emacs/site-lisp/"))
+  (normal-top-level-add-subdirs-to-load-path))
+
+;; Some people have reported problems when pasting from other OS X applications if they have set the command key as Meta, because Emacs always uses its internal killring for C-y etc. You may merge the Emacs kill-ring with the clipboard via:
+(setq x-select-enable-clipboard t)
+
+
 ;; =============================================================================
 ;; 새로운 프레임 생성시 크기 설정
 ;; =============================================================================
-(setq initial-frame-alist '((width . 120) (height . 74))) ;; 첫번째 프래임의 크기를 설정
+(setq initial-frame-alist '((width . 120) (height . 75))) ;; 첫번째 프래임의 크기를 설정
 (setq default-frame-alist '((width . 80) (height . 40))) ;; 그 다음 프래임의 크기를 설정
 
-; (setq load-path (nconc '("/Volumes/PDS/Emacs") load-path))	;; 개인 lisp 패키지가 위치 할 load-path 설정
-; (setq load-path (nconc '("~/.emacs") load-path))	;; 개인 lisp 패키지가 위치 할 load-path 설정
-(add-to-list 'load-path "/Volumes/PDS/Emacs")
-; (setq load-path (cons '("/Volumes/PDS/Emacs") load-path))
+;; (setq load-path (nconc '("~/.emacs") load-path))	;; 개인 lisp 패키지가 위치 할 load-path 설정
+(setq load-path (nconc '("~/.emacs.d/lisp") load-path))	;; 개인 lisp 패키지가 위치 할 load-path 설정
+(setq load-path (nconc '("/usr/local/opt/global/share/gtags") load-path))
+; (setq load-path (nconc '("/Volumes/PDS/Users/x/Works/ggtags") load-path))
+
 
 (setq frame-title-format
       (list (format "%s %%S: %%j " (system-name))
         '(buffer-file-name "%f" (dired-directory dired-directory "%b"))))
-
-
-(defun set-win-key-bindings ()
-  "Windows Keyboard key bindings"
-  (interactive)
-  (setq mac-option-modifier 'alt)
-  (setq mac-command-modifier 'meta)
-  (global-set-key [kp-delete] 'delete-char) ;; sets fn-delete to be right-delete
-  )
 
 (defun show-file-name ()
   "Show the full path file name in the minibuffer."
@@ -35,16 +44,15 @@
 
 ; for cscope
 ; (require 'xcscope)
-; (setq cscope-do-not-update-database t)
-; (add-hook 'java-mode-hook (function cscope:hook))
-;
-; (require 'hanja-util)
+(require 'gtags)
+;(require 'ggtags)
+(setq cscope-do-not-update-database t)
 
 ;; =============================================================================
 ;; person info
 ;; =============================================================================
 (setq user-full-name "Nate Doohyun Jang")
-(setq user-mail-address "dh.jang@gmail.com")
+(setq user-mail-address "lum7671@me.com")
 
 ;; =============================================================================
 ;; 기본 색 지정
@@ -61,10 +69,7 @@
 ;; =============================================================================
 ;; 한글 세벌식 최종
 ;; =============================================================================
-;; (require 'hangul)
 (set-input-method "korean-hangul3f") ;; if you want 3 beolsik final...
-;; (utf-translate-cjk-mode 1)
-(prefer-coding-system 'utf-8)
 
 ;;모드라인에 현재 커서의 줄과 칼럼위치 시간 표시('mode line hacking'때문에 필요 없음)
 (setq column-number-mode t)             ;;컬럼수를 모드라인에 표시할 경우 에디팅 속도가 느려진다
@@ -135,7 +140,7 @@
                ("\\.cgi\\'" . cperl-mode)
                ("\\.pl\\'" . cperl-mode)
                ("\\.pm\\'" . cperl-mode)
-               ("\\.py\\'" . phthon-mode)
+               ("\\.py\\'" . python-mode)
                ("\\.c\\'" . c-mode)
                ("\\.C\\'" . c-mode)
                ("\\.cc\\'" . c++-mode)
@@ -155,24 +160,20 @@
 ;; ================================================================================
 (add-hook 'c-mode-common-hook
           '(lambda()
-            (c-set-style "java") ;;C 코딩 스타일 정의
+            (c-set-style "k&r") ;;C 코딩 스타일 정의
 			(gtags-mode 1)
-			(when (derived-mode-p 'c-mode 'c++-mode 'java-mode)
-              (ggtags-mode 1))
 ))
 
 (add-hook 'c++-mode-common-hook
           '(lambda()
-            (c-set-style "java") ;;C 코딩 스타일 정의
+            (c-set-style "k&r") ;;C 코딩 스타일 정의
 			(gtags-mode 1)
-			(when (derived-mode-p 'c-mode 'c++-mode 'java-mode)
-              (ggtags-mode 1))
 ))
 
 ;; revert buffer. 파일 내용이나 디렉토리 내용이 바뀌어서 Disk의 내용을 다시 불러올때
 ;; g : dired 모드에서 사용
 ;; M-x revert-buffer RET yes RET
-;; C-x C-v RET		
+;; C-x C-v RET
 
 (defun revert-all-buffers()
   "Refreshs all open buffers from their respective files"
@@ -180,7 +181,7 @@
   (let* ((list (buffer-list))
 		 (buffer (car list)))
 	(while buffer
-	  (if (string-match "\\*" (buffer-name buffer)) 
+	  (if (string-match "\\*" (buffer-name buffer))
 	      (progn
 	        (setq list (cdr list))
 	        (setq buffer (car list)))
@@ -203,10 +204,10 @@
 ;; ================================================================================
 ;; ;;indent with just spaces 들여쓰기 설정
 ;; ================================================================================
-(setq-default indent-tabs-mode nil)   ;; nil 이 아니면 들여쓰기 명령이 탭문자를 입력한다.(버퍼지역변수)
-; (setq-default indent-tabs-mode t)		;; nil 이 아니면 들여쓰기 명령이 탭문자를 입력한다.(버퍼지역변수)
-; (setq tab-stop-list '(4 8 12 16 20 24 28 32 26 40 44 48 52 56 60 64 68 72 76 80 84 88 92 96 100 104 108 112 116 120))
-(setq default-tab-width 2)              ;; 탭간 간격(버퍼지역변수)
+;;(setq-default indent-tabs-mode nil)   ;; nil 이 아니면 들여쓰기 명령이 탭문자를 입력한다.(버퍼지역변수)
+(setq-default indent-tabs-mode t)		;; nil 이 아니면 들여쓰기 명령이 탭문자를 입력한다.(버퍼지역변수)
+(setq tab-stop-list '(4 8 12 16 20 24 28 32 26 40 44 48 52 56 60 64 68 72 76 80 84 88 92 96 100 104 108 112 116 120))
+(setq default-tab-width 4)              ;; 탭간 간격(버퍼지역변수)
 ;; automatically indenting yanked text if in programming-modes
 ;; 붙여넣기 하면서 자동 들여쓰기 함수
 (defadvice yank (after indent-region activate)
@@ -225,197 +226,155 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(column-number-mode t)
+ '(cua-mode t nil (cua-base))
+ '(font-use-system-font t)
  '(show-paren-mode t))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:family "NanumGothicCoding" :foundry "nil" :slant normal :weight normal :height 141 :width normal)))))
+ '(default ((t (:family "Hack" :foundry "unknown" :slant normal :weight normal :height 120 :width normal)))))
 
-;; http://www.emacswiki.org/emacs/CollectionOfEmacsDevelopmentEnvironmentTools
-;; 6. While I was at it, I installed Exuberant Ctags using MacPorts and enabled it in CEDET::
-;;
-;(semantic-load-enable-primary-exuberent-ctags-support)
-
-;(require 'semantic/ia)
-;(require 'semantic/bovine/gcc)
-
-; (load-file "/Applications/MacPorts/EmacsMac.app/Contents/Resources/lisp/cedet/cedet.elc")
-;(global-ede-mode 1)                      ; Enable the Project management system
-;(require 'semantic/sb)
-;(semantic-mode 1)
-; (semantic-load-enable-code-helpers)      ; Enable prototype help and smart completion 
-; (global-srecode-minor-mode 1)            ; Enable template insertion menu
-
-;(setq-mode-local c-mode semanticdb-find-default-throttle
-;                 '(project unloaded system recursive))
-
-
-;; if you want to enable support for gnu global
-;(when (cedet-gnu-global-version-check t)
-;  (semanticdb-enable-gnu-global-databases 'c-mode)
-;  (semanticdb-enable-gnu-global-databases 'c++-mode))
-
-;; enable ctags for some languages:
-;;  Unix Shell, Perl, Pascal, Tcl, Fortran, Asm
-;(when (cedet-ectag-version-check)
-;  (semantic-load-enable-primary-exuberent-ctags-support))
-
-(add-to-list 'load-path "/Users/x/.emacs/dash-at-point")
+(add-to-list 'load-path "~/.emacs.d/.cask/24.5.1/elpa/dash-at-point-20140626.35")
 (autoload 'dash-at-point "dash-at-point"
   "Search the word at point with Dash." t nil)
 (global-set-key "\C-cd" 'dash-at-point)
 
-;(add-to-list 'dash-at-point-mode-alist '(perl-mode . "perl"))
-;(add-to-list 'dash-at-point-mode-alist '(c++-mode . "cpp"))
-;(add-to-list 'dash-at-point-mode-alist '(c-mode . "c"))
+;; ================================================================================
+;; 검색및 치환을 할때 찾은 문자열을 표시되게 함
+;; ================================================================================
+(setq search-highlight t)
+(setq query-replace-highlight t)
 
-;(add-hook 'rinari-minor-mode-hook
-;          (lambda () (setq dash-at-point-docset "rails")))
-
-;; like vi's %
-;; By an unknown contributor
-
-(global-set-key "%" 'match-paren)
-
-(defun match-paren (arg)
-  "Go to the matching paren if on a paren; otherwise insert %."
+										; One of my recent favorite pieces of Emacs configuration. The % command was one of the things I missed the
+										; most in Emacs, until I found this little gem. I modified it from its original source by adding
+										; previous-line and next-line to the list of commands.
+										; From http://www.emacswiki.org/emacs/ParenthesisMatching#toc4
+(defun goto-match-paren (arg)
+  "Go to the matching parenthesis if on parenthesis AND last command is a movement command, otherwise insert %.
+vi style of % jumping to matching brace."
   (interactive "p")
-  (cond ((looking-at "\\s\(") (forward-list 1) (backward-char 1))
-		((looking-at "\\s\)") (forward-char 1) (backward-list 1))
-		(t (self-insert-command (or arg 1)))))
+  (message "%s" last-command)
+  (if (not (memq last-command '(
+                                set-mark
+                                cua-set-mark
+                                goto-match-paren
+                                down-list
+                                up-list
+                                end-of-defun
+                                beginning-of-defun
+                                backward-sexp
+                                forward-sexp
+                                backward-up-list
+                                forward-paragraph
+                                backward-paragraph
+                                end-of-buffer
+                                beginning-of-buffer
+                                backward-word
+                                forward-word
+                                mwheel-scroll
+                                backward-word
+                                forward-word
+                                mouse-start-secondary
+                                mouse-yank-secondary
+                                mouse-secondary-save-then-kill
+                                move-end-of-line
+                                move-beginning-of-line
+                                backward-char
+                                forward-char
+                                scroll-up
+                                scroll-down
+                                scroll-left
+                                scroll-right
+                                mouse-set-point
+                                next-buffer
+                                previous-buffer
+								previous-line
+								next-line
+                                )
+                 ))
+      (self-insert-command (or arg 1))
+    (cond ((looking-at "\\s\(") (forward-list 1) (backward-char 1))
+          ((looking-at "\\s\)") (forward-char 1) (backward-list 1))
+          (t (self-insert-command (or arg 1))))))
+(global-set-key (kbd "%") 'goto-match-paren)
 
-;; ISO 8601 Format yyyy-mm-dd, http://ergoemacs.org/emacs/elisp_datetime.html 
-(defun insert-date ()
-  "Insert current date yyyy-mm-dd."
-  (interactive)
-  (when (region-active-p)
-    (delete-region (region-beginning) (region-end) )
-    )
-  (insert (format-time-string "%Y-%m-%d"))
+;; Make Command act as Meta, Option as Alt,
+;; right-Option as Super
+; (setq mac-command-modifier 'alt)
+; (setq mac-option-modifier 'meta)
+(setq mac-right-option-modifier 'super)
+
+;; Key bindings
+;;(osx-key-mode -1)  ; no Mac-specific key bindings
+;; (setq x-alt-keysym 'meta)
+;; key bindings
+(when (eq system-type 'darwin) ;; mac specific settings
+  (setq mac-option-modifier 'alt)
+  (setq mac-command-modifier 'meta)
+  (global-set-key [kp-delete] 'delete-char) ;; sets fn-delete to be right-delete
   )
 
-;; example of setting env var named “path”, by appending a new path to existing path
-(setenv "PATH"
-		(concat
-		 "/usr/local/bin" ":"
-		 "/usr/local/sbin" ":"
-		 (getenv "PATH")
-		 )
-		)
+;; If you want to use the option key to enter special characters (such as £) instead of functioning as Alt of Meta, you can specify this in your ~/.emacs:
+; (setq mac-option-modifier nil)
 
-(setq tramp-default-method "ssh")
-
-;; for gtags
-
-(defun gtags-root-dir ()
-  "Returns GTAGS root directory or nil if doesn't exist."
-  (with-temp-buffer
-	(if (zerop (call-process "global" nil t nil "-pr"))
-		(buffer-substring (point-min) (1- (point-max)))
-	  nil)))
-
-(defun gtags-update ()
-  "Make GTAGS incremental update"
-  (call-process "global" nil nil nil "-u"))
-
-(defun gtags-update-hook ()
-  (when (gtags-root-dir)
-	(gtags-update)))
-
-(add-hook 'after-save-hook #'gtags-update-hook)
-
-(defun gtags-update-single(filename)  
-  "Update Gtags database for changes in a single file"
-  (interactive)
-  (start-process "update-gtags" "update-gtags" "bash" "-c" (concat "cd " (gtags-root-dir) " ; gtags --single-update " filename )))
-
-(defun gtags-update-current-file()
-  (interactive)
-  (defvar filename)
-  (setq filename (replace-regexp-in-string (gtags-root-dir) "." (buffer-file-name (current-buffer))))
-  (gtags-update-single filename)
-  (message "Gtags updated for %s" filename))
-
-;(defun gtags-update-hook()
-;  "Update GTAGS file incrementally upon saving a file"
-;  (when ggtags-mode
-;	(when (gtags-root-dir)
-;	  (gtags-update-current-file))))
-;
-;(add-hook 'after-save-hook 'gtags-update-hook)
+;; if you instead decide to leave osx-key-mode on:
 
 
-(autoload 'gtags-mode "gtags" "" t)
-; (autoload 'gtags-mode "gtags" "" t)
+(add-hook 'java-mode-hook (function cscope:hook))
 
-;; (add-hook 'gtags-mode-hook
-;;   '(lambda ()
-;;      (define-key gtags-mode-map "\C-f" 'scroll-up)
-;;      (define-key gtags-mode-map "\C-b" 'scroll-down)
-;; ))
+(when (eq system-type 'darwin)
 
-(add-hook 'gtags-select-mode-hook
-  '(lambda ()
-     (setq hl-line-face 'underline)
-     (hl-line-mode 1)
-))
+  ;; default Latin font (e.g. Consolas)
+  (set-face-attribute 'default nil :family "Hack")
 
-(setq gtags-suggested-key-mapping t)
-(setq gtags-auto-update t)
+  ;; default font size (point * 10)
+  ;;
+  ;; WARNING!  Depending on the default font,
+  ;; if the size is not supported very well, the frame will be clipped
+  ;; so that the beginning of the buffer may not be visible correctly.
+  (set-face-attribute 'default nil :height 120)
 
-;(add-hook 'c-mode-common-hook
-;          (lambda ()
-;            (when (derived-mode-p 'c-mode 'c++-mode 'java-mode)
-;              (ggtags-mode 1))))
+  ;; use specific font for Korean charset.
+  ;; if you want to use different font size for specific charset,
+  ;; add :size POINT-SIZE in the font-spec.
+  (set-fontset-font t 'hangul (font-spec :name "NanumGothicCoding"))
 
-;(add-to-list 'load-path "/Volumes/PDS/Works/GitHub/go-mode.el")
-;(require 'go-mode-autoloads)
+  ;; you may want to add different for other charset in this way.
+  )
 
-(setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
-                         ("marmalade" . "https://marmalade-repo.org/packages/")
-                         ("melpa" . "http://melpa.milkbox.net/packages/")))
+(add-hook 'c-mode-common-hook
+          (lambda ()
+            (when (derived-mode-p 'c-mode 'c++-mode 'java-mode)
+              (ggtags-mode 1))))
 
-(add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t) ; Org-mode's repository
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; https://github.com/fgallina/python.el
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
+(add-to-list 'load-path "~/.emacs.d/lisp") ;; user-emacs-directory)
+(autoload 'python-mode "python-mode" "Python Mode." t)
+(add-to-list 'auto-mode-alist '("\\.py\\'" . python-mode))
+(add-to-list 'interpreter-mode-alist '("python" . python-mode))
 
-;(add-to-list 'load-path "/Volumes/PDS/Emacs/auto-complete-1.3.1")
-;(require 'auto-complete)
-;(require 'auto-complete-config)
-;(define-key ac-mode-map (kbd "M-TAB") 'auto-complete)
+;; Global font-lock mode 
+(global-font-lock-mode 1) 
+;; 
+(init-loader-load "~/.emacs.d/init-loader/")
 
-; (add-to-list 'load-path "~/Library/Preferences/Aquamacs Emacs/Packages/elpa/go-autocomplete-20141210.1904/")
-; (require 'go-autocomplete)
+;; Pymacs
+(autoload 'pymacs-apply "pymacs")
+(autoload 'pymacs-call "pymacs")
+(autoload 'pymacs-eval "pymacs" nil t)
+(autoload 'pymacs-exec "pymacs" nil t)
+(autoload 'pymacs-load "pymacs" nil t)
+(autoload 'pymacs-autoload "pymacs")
 
+;; https://github.com/jorgenschaefer/elpy
+(package-initialize)
+; (elpy-enable)
 
-(add-to-list 'load-path "~/.emacs.d/lisp")
-(autoload 'gtags-mode "gtags" "" t)
-(add-hook 'gtags-mode-hook
-  '(lambda ()
-        ; Local customization (overwrite key mapping)
-        (define-key gtags-mode-map "\C-f" 'scroll-up)
-        (define-key gtags-mode-map "\C-b" 'scroll-down)
-))
-(add-hook 'gtags-select-mode-hook
-  '(lambda ()
-        (setq hl-line-face 'underline)
-        (hl-line-mode 1)
-))
-(add-hook 'c-mode-hook
-  '(lambda ()
-        (gtags-mode 1)))
-; Customization
-(setq gtags-suggested-key-mapping t)
-(setq gtags-auto-update t)
-
-; (source gnu)
-; (source melpa) ;;archive of VCS snapshots built automatically from upstream repositories
-
-(require 'cask "~/.cask/cask.el")
-(cask-initialize)
-(require 'pallet)
-(pallet-mode t)
-
-; (mapc 'load (directory-files "~/.emacs.d/customizations" t "^[0-9]+.*\.el$"))
-
+(require 'package)
+(add-to-list 'package-archives
+             '("elpy" . "http://jorgenschaefer.github.io/packages/"))
